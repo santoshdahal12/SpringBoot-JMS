c5ff969acc07090a1df3c10ca10d68a82a5d2fb6
DESKTOP-EQ6GNNV\santosh dahal <santoshdahal2072@gmail.com>
2017-01-18 20:52:44 -0700

    first commit

---
+++ goodpersonpublisher/src/main/java/myproject/goodperson/GoodpersonpublisherApplication.java
@@ -0,0 +1,12 @@
+package myproject.goodperson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodpersonpublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodpersonpublisherApplication.class, args);
+	}
+}
